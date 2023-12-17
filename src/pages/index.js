9d7025c0e075f9abba2313a231d038b3ffcaf870
Student <student@Nests-iMac.local>
2023-12-17 11:54:40 +0800

    nav bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,4 @@
 import { useRouter } from "next/router";
-import {
-  Dropdown,
-  Ripple,
-  initTE,
-} from "tw-elements";
-
-initTE({ Dropdown, Ripple });
 
 export default function Home() {
   const router = useRouter();
@@ -14,10 +7,12 @@ export default function Home() {
     <div className="h-full w-full bg-zinc-800 flex flex-col overflow-x-hidden">
       <div className="w-full h-12 bg-zinc-800 items-center flex flex-row p-4 fixed justify-between 2xl:px-64 xl:px-52 lg:px-40 md:px-28 sm:px-16 px-8 z-50">
         <div className="flex flex-row justify-between items-center xl:2xl:gap-12 sm:lg:gap-8 gap-4">
-          <img
-            className="w-16"
-            src="https://m-bank.mn/icons/navbar/ic_m_large.png"
-          ></img>
+          <button onClick={() => router.push("/")} className="w-16">
+            <img
+              className="w-16"
+              src="https://m-bank.mn/icons/navbar/ic_m_large.png"
+            ></img>
+          </button>
           <a className="text-zinc-500 cursor-pointer text-sm hover:text-emerald-400 2xl:block xl:block lg:block md:block hidden">
             Танд
           </a>
@@ -42,81 +37,20 @@ export default function Home() {
           >
             Нэвтрэх
           </button>
-          <div class="relative" data-te-dropdown-ref>
-            <a
-              class="flex items-center whitespace-nowrap rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] motion-reduce:transition-none dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-              href="#"
-              type="button"
-              id="dropdownMenuButton2"
-              data-te-dropdown-toggle-ref
-              aria-expanded="false"
-              data-te-ripple-init
-              data-te-ripple-color="light"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-6 h-6 stroke-white"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-
-              <span class="ml-2 w-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  class="h-5 w-5"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </span>
-            </a>
-            <ul
-              class="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
-              aria-labelledby="dropdownMenuButton2"
-              data-te-dropdown-menu-ref
-            >
-              <li>
-                <a
-                  class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-                  href="#"
-                  data-te-dropdown-item-ref
-                >
-                  Action
-                </a>
-              </li>
-              <li>
-                <a
-                  class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-                  href="#"
-                  data-te-dropdown-item-ref
-                >
-                  Another action
-                </a>
-              </li>
-              <li>
-                <a
-                  class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-                  href="#"
-                  data-te-dropdown-item-ref
-                >
-                  Something else here
-                </a>
-              </li>
-            </ul>
-          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6 stroke-white 2xl:hidden xl:hidden lg:hidden md:hidden block"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
         </div>
       </div>
 
